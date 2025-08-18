bc6a1625884dbdb6e03da3f6298d38b465c5adf5
3m <3m@gmail.com>
2025-08-18 23:20:30 +0300

    Initial commit: React Vite + Tailwind

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import { useState } from "react";
+
+import "./App.css";
+
+function App() {
+  return (
+    <>
+      <main className="min-h-screen flex items-center justify-center bg-gray-50">
+        <h1 className="text-4xl font-bold">DSC Developer Students Club</h1>
+      </main>
+    </>
+  );
+}
+
+export default App;
